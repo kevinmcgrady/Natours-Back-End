65a4189fd9d61f33710c377c07b0b67da8072cb8
kevinmcgrady <kevinmcgrady47@gmail.com>
2020-02-29 22:18:10 +0000

    added password validation, password hashing

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new Schema(
   {
@@ -24,6 +25,12 @@ const userSchema = new Schema(
     passwordConfirm: {
       type: String,
       required: [true, 'Please confirm your password'],
+      validate: {
+        validator: function(value) {
+          return value === this.password;
+        },
+        message: 'Both passwords must match',
+      },
     },
   },
   {
@@ -31,5 +38,18 @@ const userSchema = new Schema(
     toObject: { virtuals: true },
   },
 );
+
+// Hash password before save
+userSchema.pre('save', async function(next) {
+  // If password is not modified, return and call next middleware.
+  if (!this.isModified('password')) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+
+  // Delete the password confirm field - no longer needed
+  this.passwordConfirm = undefined;
+
+  next();
+});
 
 module.exports = mongoose.model('User', userSchema);
