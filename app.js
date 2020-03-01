c9916af8eb4ebd86f55f4a1fc41f2402dd293bb0
kevinmcgrady <kevinmcgrady47@gmail.com>
2020-03-01 19:32:48 +0000

    added packages to prevent xss and NoSQL injectinon

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const morgan = require('morgan');
+const hemlet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+
 const { limiter } = require('./utils/limiter');
 
 const { errorHandeler } = require('./middleware/errorMiddleware');
@@ -13,14 +17,27 @@ const tourRouter = require('./routes/tourRoutes');
 const app = express();
 
 // Middleware.
-app.use(express.json());
+// Set security HTTP headers
+app.use(hemlet());
 
+// Allow JSON body data into req.body and limit to 10kb
+app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against NoSQL query injection.
+app.use(mongoSanitize());
+
+// Data sanitization against XSS.
+app.use(xss());
+
+// Limit requests for a single IP on all API routes.
 app.use('/api', limiter);
 
+// Development logging.
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// Serving static files from public folder.
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
