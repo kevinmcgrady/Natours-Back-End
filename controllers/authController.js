74a37cb977282d3e748d69b424b009f2c5513cf9
kevinmcgrady <kevinmcgrady47@gmail.com>
2020-03-01 09:36:47 +0000

    added signin on sugnup with JWT

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,24 @@
 const User = require('../models/User');
 const { catchAsync } = require('../middleware/errorMiddleware');
+const jwt = require('jsonwebtoken');
 
 module.exports.signup = catchAsync(async (req, res, next) => {
-  const userData = req.body;
-  const newUser = await User.create(userData);
+  // Create new user.
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
+
+  // Create token.
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
 
   res.status(201).json({
     status: 'success',
+    token,
     data: {
       user: newUser,
     },
