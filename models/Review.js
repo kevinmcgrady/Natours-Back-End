887b568f2889a41dd9842545f5ab45b3c6ac9d64
Kevin McGrady <kevinmcgrady47@gmail.com>
2020-03-07 17:58:46 +0000

    added review model

---
+++ models/Review.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const reviewSchema = new Schema(
+  {
+    review: {
+      type: String,
+      required: [true, 'A review must have a review body'],
+    },
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+      required: [true, 'A review must have a rating'],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
+    tour: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Tour',
+      required: [true, 'A review must belong to a tour'],
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: [true, 'A review must belong to a user'],
+    },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  },
+);
+
+module.exports = mongoose.model('Review', reviewSchema);
