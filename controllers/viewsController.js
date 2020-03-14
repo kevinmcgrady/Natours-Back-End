2a6923afce02b6d3ee2060e6bc3683c21d91b84d
Kevin McGrady <kevinmcgrady47@gmail.com>
2020-03-14 11:26:08 +0000

    created overview page

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,8 +1,14 @@
-exports.getOverview = (req, res, next) => {
+const Tour = require('../models/Tour');
+const { catchAsync } = require('../middleware/errorMiddleware');
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+  const tours = await Tour.find();
+
   res.status(200).render('overview', {
     title: 'All Tours',
+    tours,
   });
-};
+});
 
 exports.getTour = (req, res, next) => {
   res.status(200).render('tour', {
