d1c625befeba2ce58c93df607bc7af8ecb61a1f4
Kevin McGrady <kevinmcgrady47@gmail.com>
2020-03-21 22:58:54 +0000

    created email sending class

--- utils/emailHandeler.js
+++ utils/emailHandeler.js
@@ -1,22 +1,56 @@
 const nodemailer = require('nodemailer');
+const pug = require('pug');
+const htmlToText = require('html-to-text');
 
-module.exports.sendEmail = async options => {
-  // create transporter.
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
-  // define the email options.
-  const mailOptions = {
-    from: 'Kevin Mcgrady <kevinmcgrady47@gmail.com>',
-    to: options.email,
-    subject: options.subject,
-    text: options.message,
-  };
-  // send the email.
-  await transporter.sendMail(mailOptions);
+module.exports = class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstName = user.name.split(' ')[0];
+    this.url = url;
+    this.from = `Kevin Mcgrady <${process.env.EMAIL_FROM}>`;
+  }
+
+  createTransport() {
+    if (process.env.NODE_ENV === 'production') {
+      return 1;
+    }
+
+    // create transporter.
+    return (transporter = nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    }));
+  }
+
+  async send(template, subject) {
+    const html = pug.renderFile(
+      `${__dirname}/../views/emails/${template}.pug`,
+      {
+        firstName: this.firstName,
+        url: this.url,
+        subject,
+      },
+    );
+
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject: this.subject,
+      html,
+      text: htmlToText.fromString(html),
+    };
+
+    await this.createTransport().sendMail(mailOptions);
+  }
+
+  async sendWelcome() {
+    await this.send(
+      'welcome',
+      `Welcome to the Natours Family, ${this.firstName}`,
+    );
+  }
 };
